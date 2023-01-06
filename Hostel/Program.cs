a2ebec8ebe9aa93b0e3df64dcbe6cf45dbe76845
eugene-coding <eugene.code@yandex.ru>
2023-01-06 05:29:17 +0300

    Initial commit
    Removed everything unneeded

---
+++ Hostel/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
