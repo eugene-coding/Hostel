2edd539741d355e2699ff8ccb56920968ad5524b
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-08-24 19:08:53 +0300

    Added context and concert table

---
+++ Database/Models/Concert.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Database.Models;
+
+/// <summary>
+/// Представляет таблицу с концертами.
+/// </summary>
+[Comment("Концерты.")]
+public sealed class Concert
+{
+    /// <summary>
+    /// ID концерта.
+    /// </summary>
+    [Comment("ID концерта.")]
+    public uint Id { get; set; }
+
+    /// <summary>
+    /// Название концерта.
+    /// </summary>
+    [MaxLength(100)]
+    [Comment("Название концерта.")]
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Город проведения.
+    /// </summary>
+    [MaxLength(100)]
+    [Comment("Город проведения.")]
+    public required string City { get; set; }
+
+    /// <summary>
+    /// Место проведения.
+    /// </summary>
+    [MaxLength(100)]
+    [Comment("Место проведения.")]
+    public required string Location { get; set; }
+
+    /// <summary>
+    /// Время проведения.
+    /// </summary>
+    [Comment("Время проведения.")]
+    public DateTime DateTime { get; set; }
+
+    /// <summary>
+    /// Количество оставшихся билетов.
+    /// </summary>
+    [Comment("Количество оставшихся билетов.")]
+    public ushort TicketsLeft { get; set; }
+
+    /// <summary>
+    /// Минимальная цена билета.
+    /// </summary>
+    [Comment("Минимальная цена билета.")]
+    public ushort MinPrice { get; set; }
+}
