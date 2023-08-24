ae8a1c453a8a29a4e35f3bb5aad9eae532d4e000
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-08-24 19:49:49 +0300

    Added seed data

--- Database/Context.cs
+++ Database/Context.cs
@@ -21,4 +21,30 @@ public sealed class Context : DbContext
     /// Таблица с концертами.
     /// </summary>
     public DbSet<Concert> Concerts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Concert>().HasData(
+            new Concert
+            {
+                Id = 1,
+                City = "Волгоград",
+                Location = "Белая лошадь",
+                Name = "Сольный концерт",
+                DateTime = DateTime.Now,
+                TicketsLeft = 47,
+                MinPrice = 2500,
+            },
+
+            new Concert
+            {
+                Id = 2,
+                City = "Москва",
+                Location = "Тонна",
+                Name = "Лазурный рассвет",
+                DateTime = new DateTime(2023, 12, 24, 19, 0, 0),
+                TicketsLeft = 32,
+                MinPrice = 1600,
+            });
+    }
 }
