26a2b99b2070994c2fe0c8db3f87e6aa9704f332
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-08-25 01:21:56 +0300

    Added setting service

---
+++ Core/Interfaces/ISettingService.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Interfaces;
+
+/// <summary>
+/// Сервис для получения настроек приложения.
+/// </summary>
+public interface ISettingService
+{
+    /// <summary>
+    /// Возвращает количество концертов, получаемых в рамках одной страницы. 
+    /// </summary>
+    /// <returns>
+    /// Число, определяющее, сколько концертов нужно загрузить в рамках одной страницы.
+    /// </returns>
+    Task<int> GetConcertsPerPageAsync();
+}
