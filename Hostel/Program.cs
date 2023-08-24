408157c055e28e7fb601cb0a74f7756487394d3a
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-08-24 23:35:24 +0300

    Configured DB connection

--- Hostel/Program.cs
+++ Hostel/Program.cs
@@ -5,10 +5,31 @@ using Database.Services;
 
 using Hostel;
 
+using Microsoft.EntityFrameworkCore;
+
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddServerComponents();
+
+builder.Services.AddDbContext<Context>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Database");
+
+    options.UseSqlServer(connectionString, options =>
+    {
+        options.EnableRetryOnFailure()
+            .MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+    });
+
+    if (builder.Environment.IsDevelopment())
+    {
+        options.EnableDetailedErrors()
+            .EnableSensitiveDataLogging();
+    }
+});
 
 var app = builder.Build();
 
