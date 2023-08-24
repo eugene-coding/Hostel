8d4d35d05766d2e9b19a93ffccc27bd03be3264d
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-08-24 19:08:53 +0300

    Added context and concert table

---
+++ Database/Context.cs
@@ -0,0 +1,24 @@
+﻿using Database.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Database;
+
+/// <summary>
+/// Контекст базы данных.
+/// </summary>
+public sealed class Context : DbContext
+{
+    /// <summary>
+    /// Создаёт экземпляр класса.
+    /// </summary>
+    /// <param name="options">Опции для настройки контекста.</param>
+    public Context(DbContextOptions options) : base(options)
+    {
+    }
+
+    /// <summary>
+    /// Таблица с концертами.
+    /// </summary>
+    public DbSet<Concert> Concerts { get; set; }
+}
