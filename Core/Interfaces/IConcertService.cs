f2008490e12e11000e42cf82e0303e576c49a26d
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-08-24 23:31:03 +0300

    Create IConcertService.cs

---
+++ Core/Interfaces/IConcertService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Models;
+
+namespace Core.Interfaces;
+
+/// <summary>
+/// Сервис для работы с информацией о концертах.
+/// </summary>
+public interface IConcertService
+{
+    /// <summary>
+    /// Вовзаращает список предстоящих концертов.
+    /// </summary>
+    /// <returns>Список предстоящих концертов.</returns>
+    IAsyncEnumerable<ConcertModel> GetConcertModelsAsync();
+
+    /// <summary>
+    /// Подсчитывает количество предстоящих концертов.
+    /// </summary>
+    /// <returns>Количество предстоящих концертов.</returns>
+    Task<int> GetCountAsync();
+}
