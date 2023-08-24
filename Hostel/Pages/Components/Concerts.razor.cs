1fc2ad18815a940da4bf34598e4dfbe53959f468
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-08-24 18:43:45 +0300

    Change layout for using ConcertView

---
+++ Hostel/Pages/Components/Concerts.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace Hostel.Pages.Components;
+
+public partial class Concerts
+{
+    private readonly IEnumerable<ConcertView> _concerts = new List<ConcertView>
+    {
+        new ConcertView(DateTime.Now, 2400)
+        {
+            City = "Волгоград",
+            Location = "Белая Лошадь",
+            Name = "Сольный концерт",
+            TicketsLeft = 45,
+        },
+
+        new ConcertView(new DateTime(2022,10,12,20,00, 00), 2500)
+        {
+            City = "Москва",
+            Location = "Тонна",
+            Name = "Сольный концерт",
+            TicketsLeft = 70,
+        },
+    };
+}
