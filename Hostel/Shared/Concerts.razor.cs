24d63b5c2aabce92c1664c3447ec5782f12261ef
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-08-27 18:41:50 +0300

    Update Concerts.razor.cs

--- Hostel/Shared/Concerts.razor.cs
+++ Hostel/Shared/Concerts.razor.cs
@@ -2,6 +2,7 @@
 using Core.Models;
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.Localization;
 
 namespace Hostel.Shared;
@@ -13,87 +14,113 @@ public partial class Concerts
 {
     private readonly List<ConcertModel> _concerts = new();
     private readonly List<string> _cities = new();
-    private readonly ConcertFilter _filter = new();
-    private readonly string _minDate = DateTime.Now.ToString("yyyy-MM-dd");
 
+    private ConcertFilter _filter = null!;
     private bool _displayLoadMoreButton;
-    private int _concertsCount;
+    private int _totalConcertsCount;
     private int _pageToLoad = 1;
+    private string _today = string.Empty;
 
     [Inject] private IStringLocalizer<Concerts> Localizer { get; init; } = null!;
     [Inject] private IConcertService Service { get; init; } = null!;
 
     /// <inheritdoc/>
+    protected override void OnInitialized()
+    {
+        var today = DateTime.Now.Date;
+
+        _today = today.ToString("yyyy-MM-dd");
+        _filter = new ConcertFilter(today, OnFilterChanged);
+    }
+
+    /// <inheritdoc/>
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        if (firstRender)
-        {
-            _concertsCount = await Service.GetCountAsync();
-
-            await GetCities();
-            await GetNextPageAsync();
-
-            StateHasChanged();
-        }
-    }
-
-    private async Task GetCities()
-    {
-        await foreach (var city in Service.GetCities())
-        {
-            _cities.Add(city);
-        }
-    }
-
-    private async Task GetNextPageAsync()
-    {
-        await foreach (var concert in Service.GetConcertModelsAsync(_pageToLoad++))
-        {
-            _concerts.Add(concert);
-        }
-
-        _displayLoadMoreButton = _concertsCount > _concerts.Count;
-    }
-
-    private void OnCityChanged(ChangeEventArgs e)
-    {
-        var city = e.Value?.ToString();
-
-        if (_filter.City != city)
-        {
-            _filter.City = city;
-
-            _concerts.Clear();
-        }
-    }
-
-    private void OnFromChanged(ChangeEventArgs e)
-    {
-        if (!DateTime.TryParse(e.Value?.ToString(), out var from))
+        if (!firstRender)
         {
             return;
         }
 
-        if (_filter.From != from)
+        _totalConcertsCount = await Service.GetCountAsync();
+
+        // Концертов нет
+        if (_totalConcertsCount == 0)
+        {
+            return;
+        }
+
+        await foreach (var city in Service.GetCities())
+        {
+            _cities.Add(city);
+        }
+
+        await GetNextPageAsync();
+
+        StateHasChanged();
+    }
+
+    private async Task GetNextPageAsync()
+    {
+        await foreach (var concert in Service.GetConcertModelsAsync(_pageToLoad++, _filter))
+        {
+            _concerts.Add(concert);
+        }
+
+        _displayLoadMoreButton = _totalConcertsCount > _concerts.Count;
+    }
+
+    private void OnCityChanged(ChangeEventArgs e)
+    {
+        var city = e.Value?.ToString() ?? string.Empty;
+        _filter.City = city;
+    }
+
+    private void OnFromChanged(ChangeEventArgs e)
+    {
+        if (DateTime.TryParse(e.Value?.ToString(), out var from))
         {
             _filter.From = from;
-
-            _concerts.Clear();
+        }
+        else
+        {
+            _filter.ResetFrom();
         }
     }
 
     private void OnToChanged(ChangeEventArgs e)
     {
-        if (!DateTime.TryParse(e.Value?.ToString(), out var to))
-        {
-            return;
-        }
-
-        if (_filter.To != to)
+        if (DateTime.TryParse(e.Value?.ToString(), out var to))
         {
             _filter.To = to;
-
-            _concerts.Clear();
         }
+        else
+        {
+            _filter.ResetTo();
+        }
+    }
+
+    private void OnClearReset(MouseEventArgs e)
+    {
+        _filter.Reset();
+    }
+
+    private async Task OnFilterChanged()
+    {
+        ClearConcerts();
+
+        _totalConcertsCount = await Service.GetCountAsync(_filter);
+
+        if (_totalConcertsCount > 0)
+        {
+            await GetNextPageAsync();
+        }
+
+        StateHasChanged();
+    }
+
+    private void ClearConcerts()
+    {
+        _concerts.Clear();
+        _pageToLoad = 1;
     }
 }
