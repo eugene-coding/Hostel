0f2ef13a3dce144f60305f347eda6bf5289befb3
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-08-29 01:12:28 +0300

    Create ConfigurationHelper.cs

---
+++ DatabaseTest/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DatabaseTest.Helpers;
+
+internal static class ConfigurationHelper
+{
+    public static string GetTestConnectionString()
+    {
+        const string connectionStringName = "TestDatabase";
+
+        return GetConfiguration().GetConnectionString(connectionStringName)
+            ?? throw new NullReferenceException(
+                $"Не удалось найти строку подключения под названием {connectionStringName}.");
+    }
+
+    private static IConfigurationRoot GetConfiguration()
+    {
+        var configBuilder = new ConfigurationBuilder();
+        configBuilder.AddJsonFile("appsettings.json", false, true);
+
+        return configBuilder.Build();
+    }
+}
